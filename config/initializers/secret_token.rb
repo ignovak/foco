56cf35572409f5aef1a4f300f77003499d4ad96f
Igor Novak <bezengi@gmail.com>
2012-04-28 16:26:49 +0300

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Foco::Application.config.secret_token = '812078f644d7efac5cd3c667ba24235065f22be56f01ffa261660886f5979476a475341e98d82fa1c0a0ba86e40c39f71e552fd35ce2812d651c1921ad200d6f'
