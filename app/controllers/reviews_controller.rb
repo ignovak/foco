29ebe621c2da1634c1eccea54bb5560946b4e7b2
Igor Novak <bezengi@gmail.com>
2012-04-29 01:13:00 +0300

    create process listening stdin

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,13 +1,17 @@
 class ReviewsController < ApplicationController
 
+  def show
+
+  end
+
   def new
     @project = Project.find(params[:project_id])
     @review = @project.reviews.build
   end
 
   def create
-    current_user.reviews.create(params[:review])
-
-    render text: params[:review]
+    review = current_user.reviews.create(params[:review])
+    $record_on = true
+    redirect_to project_review_path(review.project_id, review.id)
   end
 end
--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,42 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Foco::Application.initialize!
+
+$record_on = false
+pig = IO.popen('bash q.sh', 'w+')
+
+Thread.new do
+  loop do
+    if $record_on
+      pig.write('start')
+      put_to_db(pig.gets)
+      started = true
+    else
+      if started
+        pig.write('stop')
+        started = false
+      end
+      sleep 0.1
+    end
+  end
+end
+
+# Thread.new do
+#   loop do
+#     if $record_on
+#       data = []
+#       10.times do
+#         if $record_on
+#           pig.write('start')
+#           data << pig.gets
+#         else
+#           pig.write('stop')
+#           break
+#         end
+#       end
+#       put_to_db(data)
+#     else
+#       sleep 0.1
+#     end
+#   end
+# end
